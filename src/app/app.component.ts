7c043085ae8363cb7decfc00bc5ac911982a03ef
Manjunath V A <manjunathappi68@gmail.com>
2019-12-31 00:23:55 +0530

    updated the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ,NgZone} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'domain-app';
+ 
+  constructor(private zone:NgZone){
+    window.addEventListener('offline', this.offlineEvent);
+    window.addEventListener('online', this.onlineEvent);
+  }
+
+  ngOnInit(){
+  // Cordova platform and plugins initialization
+  document.addEventListener('deviceready', () => {
+    this.zone.run(() => this.onCordovaReady());
+}, false);
+  }
+
+  
+  offlineEvent = ()=> {
+   console.log("offline");
+  }
+onlineEvent = ()=> {
+  console.log("online");
+}
+   
+  private onCordovaReady() {
+    if (window['cordova']) {
+       
+      
+    }
+}
 }
